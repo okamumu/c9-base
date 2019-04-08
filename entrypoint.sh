f4a9247c14772f8b1da3556e8a1dd994677f3ab9
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2019-04-08 12:34:30 +0900

    add profile and so on

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,18 @@ groupadd -f -g $C9GID $C9GROUP
 useradd -d $C9HOME -u $C9UID -g $C9GID -p `echo "$C9PASSWORD" | mkpasswd -s -m sha-512` $C9USER &&\
   echo "$C9USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/$C9USER
 
+if [ ! -d $C9HOME/.bash_logout ]; then
+  su - $C9USER -c "cp /etc/skel/.bash_logout $C9HOME/.bash_logout"
+fi
+
+if [ ! -d $C9HOME/.bashrc ]; then
+  su - $C9USER -c "cp /etc/skel/.bashrc $C9HOME/.bashrc"
+fi
+
+if [ ! -d $C9HOME/.profile ]; then
+  su - $C9USER -c "cp /etc/skel/.profile $C9HOME/.profile"
+fi
+
 ## install c9
 
 if [ ! -d $C9HOME/cloud9 ]; then
